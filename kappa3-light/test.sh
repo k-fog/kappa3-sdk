2954ae953d664534d0b0e0d39f28cc31ee9bc33b
k-fog <59814110+k-fog@users.noreply.github.com>
2025-06-16 14:48:22 +0900

    test

--- kappa3-light/test.sh
+++ kappa3-light/test.sh
@@ -5,9 +5,12 @@ assert() {
         echo "Test directory for $1 does not exist"
         exit 1
     fi
-    iverilog -c filelist.txt src/instruction_tester.v -I testbench/$1 -o testbench/$1/instruction_tester.vvp
-    vvp testbench/$1/instruction_tester.vvp > testbench/$1/result.txt
-    if ! diff testbench/$1/result.txt testbench/$1/expected.txt; then
+    mkdir -p test_tmp/$1
+
+    iverilog -c filelist.txt src/instruction_tester.v -I testbench/$1 -o test_tmp/$1/instruction_tester.vvp
+    vvp test_tmp/$1/instruction_tester.vvp > test_tmp/$1/result.txt
+
+    if ! diff test_tmp/$1/result.txt testbench/$1/expected.txt; then
         echo "Output mismatch for $1"
         exit 1
     fi
---
+++ kappa3-light/testbench/basic03_jal/test_instruction.vh
@@ -0,0 +1,2 @@
+write_pc(32'h10000000);
+write_mem(32'h10000000, 32'hF01FF1EF);
